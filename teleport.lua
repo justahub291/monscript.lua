4899b68c5a8de79c61a253c9f6d2dcf7a3f19406
justahub291 <elbazfatima781@gmail.com>
2026-02-22 23:59:45 +0100

    Create teleport.lua

---
+++ teleport.lua
@@ -0,0 +1,16 @@
+local TeleportService = game:GetService("TeleportService")
+
+local placeId = 109983668079237 -- Remplace par l'ID de ton jeu
+local jobId = "2a04c7a7-709a-4261-83c9-f69272bdcdcf"
+
+local function rejoindreServeur()
+    local success, result = pcall(function()
+        TeleportService:TeleportToPlaceInstance(placeId, jobId, game.Players.LocalPlayer)
+    end)
+    
+    if not success then
+        warn("Erreur lors de la téléportation : " .. tostring(result))
+    end
+end
+
+rejoindreServeur()
